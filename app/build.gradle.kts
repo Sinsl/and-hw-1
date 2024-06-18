b166bb6fd1877c262a5354ba0c9ba41d55e73f20
Sinsl <sinsl@me.com>
2024-06-18 12:25:32 +0300

    add lifecycle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
